c149de0e5f0544797dd0d95c078f71c5f905e353
sandeshapparala <sandeshapparala@gmail.com>
2024-10-31 23:01:58 +0530

    client side fetching

--- app/video/[videoId]/components/TranscriptTab.tsx
+++ app/video/[videoId]/components/TranscriptTab.tsx
@@ -1,14 +1,13 @@
-'use client';
 import { useEffect } from 'react';
 import axios from 'axios';
 import Loader from "@/components/Loader";
 
 const TranscriptTab = ({ videoId, transcript, setTranscript, loading, setLoading, error, setError }:
-    { videoId: string, transcript: string, setTranscript: (transcript: string) => void, loading: boolean, setLoading: (loading: boolean) => void, error: string, setError: (error: string) => void }) => {
+                           { videoId: string, transcript: string, setTranscript: (transcript: string) => void, loading: boolean, setLoading: (loading: boolean) => void, error: string, setError: (error: string) => void }) => {
 
   useEffect(() => {
     async function fetchTranscript() {
-      if (transcript) return; // If transcript is already fetched, do not fetch again
+      if (transcript) return;
       try {
         setLoading(true);
         const response = await axios.post('/api/transcript', { videoId });
@@ -24,18 +23,18 @@ const TranscriptTab = ({ videoId, transcript, setTranscript, loading, setLoading
   }, [videoId, transcript, setTranscript, setLoading, setError]);
 
   if (loading) return (
-    <div className="flex items-center justify-center h-full">
-      <Loader message="Loading transcript..." />
-    </div>
+      <div className="flex items-center justify-center h-full">
+        <Loader message="Loading transcript..." />
+      </div>
   );
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="p-4">
-      <div className="prose max-w-none text-black">
-        {transcript}
+      <div className="p-4">
+        <div className="prose max-w-none text-black">
+          {transcript}
+        </div>
       </div>
-    </div>
   );
 };
 
